65e1c1939b02cb77ac3619577b51d5e10bd85510
Blacksoup <mushiii@gmail.com>
2025-01-11 15:55:56 +1100

    Cage movement and Timewave vfx

--- fire.gd
+++ fire.gd
@@ -3,23 +3,12 @@ extends Area2D
 
 @export var descent_speed: float = 65.0  # Pixels per second
 @export var damage_amount: int = 1
+
 var light_data = []
 
 
 
 func _ready():
-	#for node in get_tree().get_nodes_in_group("boom1"):
-		#node.play("boom")
-	#await get_tree().create_timer(1.75).timeout
-	#for node in get_tree().get_nodes_in_group("boom2"):
-		#node.play("boom")
-	#await get_tree().create_timer(1.0).timeout
-	#for node in get_tree().get_nodes_in_group("boom3"):
-		#node.play("boom")
-	#await get_tree().create_timer(1.5).timeout
-	#for node in get_tree().get_nodes_in_group("boom4"):
-		#node.play("boom")
-	#print("Fire hazard ready. Monitoring: ", monitoring)
 	for light in get_children():
 		if light is PointLight2D:
 			light_data.append({
@@ -29,19 +18,6 @@ func _ready():
 				"base_intensity": light.energy
 			})
 
-	# Set up the collision shape
-	#var collision = CollisionShape2D.new()
-	#var shape = RectangleShape2D.new()
-	#shape.size = Vector2(1000, 50)  # Adjust width and height as needed
-	#collision.shape = shape
-	#add_child(collision)
-	
-	# Set up the visual representation
-	#var visual = ColorRect.new()
-	#visual.color = Color(1, 0.3, 0, 0.5)  # Semi-transparent orange
-	#visual.size = Vector2(1500, 10)  # Match collision shape size
-	#visual.position = Vector2(-500, -25)  # Center the rectangle
-	#add_child(visual)
 
 	await get_tree().create_timer(25).timeout
 	descent_speed += 20
